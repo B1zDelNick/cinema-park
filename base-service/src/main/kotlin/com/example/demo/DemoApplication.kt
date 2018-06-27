8bcc465d4c1135eb2d035843d9b5d8618286587e
avasilevich <andrey.vasilevich@pressindex.ru>
2018-06-27 14:26:27 +0700

    config server

--- base-service/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ base-service/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,10 +1,25 @@
 package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.core.env.Environment
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
-class DemoApplication
+@EnableConfigurationProperties(Props::class)
+class DemoApplication(private val props: Props) {
+    @PostConstruct
+    fun init() {
+        println("\n\n${props.test}\n\n")
+    }
+}
+
+@ConfigurationProperties
+class Props {
+    var test: String = "notSet"
+}
 
 fun main(args: Array<String>) {
     runApplication<DemoApplication>(*args)
